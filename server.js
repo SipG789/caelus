afd9f322f79c0842380d64ad6a5443121b88ed72
Sip <sipg789@gmail.com>
2022-04-29 00:23:08 -0500

    initial files set up

---
+++ server.js
@@ -0,0 +1,17 @@
+// TODO: install mysql2, inquirer, console.table packages, package json & lock.json
+
+// TODO: use .env file for passwords
+
+// TODO: make queries asynchronous 
+
+// TODO: add tables in a separate file that contains functions for performing specific SQL queries 
+// TODO: include a seeds.sql file to pre-populate databases in order to make individual features easier
+// TODO: add department table 
+// TODO: add role table
+// TODO: add employee table 
+
+
+
+
+// TODO: BONUS!!! update employee manager, view employees by managers, view employees by department,  
+    // delete departments, roles, and employees, view the total utilized budget of a dept. (combined salaries of all)
